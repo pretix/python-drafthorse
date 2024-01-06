2510362eb59904c3149c198cd6495cfe4675edf6
MAKOMO <marko.luther@gmx.net>
2024-01-06 21:14:55 +0100

    tests for en16931 auto profile recognition and auto profile recognition failure

--- tests/test_mininal.py
+++ tests/test_mininal.py
@@ -2,7 +2,6 @@ import pytest
 
 from drafthorse.pdf import attach_xml
 
-
 @pytest.mark.parametrize("invoice_document", ["380"], indirect=True)
 def test_readme_construction_example_pdf16(invoice_document, empty_pdf16_bytes):
     """
@@ -20,6 +19,21 @@ def test_readme_construction_example_pdf17(invoice_document, invoice_pdf17_bytes
     Test using a PDF 1.7 version in order to cover the output intents handling
     """
     xml = invoice_document.serialize(schema="FACTUR-X_EXTENDED")
+    output_pdf = attach_xml(invoice_pdf17_bytes, xml)
+
+    assert output_pdf
+
+
+@pytest.mark.parametrize("invoice_document", ["380"], indirect=True)
+def test_readme_construction_example_pdf17_en16931(invoice_document, invoice_pdf17_bytes):
+    """
+    Test using a PDF 1.7 version in order to cover the output intents handling
+    """
+    doc = invoice_document
+    doc.context.guideline_parameter.id = (
+        "urn:cen.eu:en16931:2017"
+    )
+    xml = doc.serialize(schema="FACTUR-X_EXTENDED")
     output_pdf = attach_xml(invoice_pdf17_bytes, xml)
 
     assert output_pdf
@@ -57,6 +71,24 @@ def test_readme_construction_example_pdf17_xrechnung3(
     output_pdf = attach_xml(invoice_pdf17_bytes, xml)
 
     assert output_pdf
+
+@pytest.mark.parametrize("invoice_document", ["380"], indirect=True)
+def test_invalid_invoice_XML_profile_exceptions(
+    invoice_document, invoice_pdf17_bytes
+):
+    """
+    Test using a PDF 1.7 version in order to cover the output intents handling
+    """
+    doc = invoice_document
+    doc.context.guideline_parameter.id = (
+        "urn:cen.eu:en16932:2017#conformant#urn:factur-x.eu:1p0:wrong"
+    )
+    xml = doc.serialize(schema="FACTUR-X_EXTENDED")
+    # invalid pdf type
+    with pytest.raises(Exception) as exc_info:
+        attach_xml(invoice_pdf17_bytes, xml)
+    
+    assert str(exc_info.value) == "Invalid XML profile!"
 
 
 @pytest.mark.parametrize("invoice_document", ["220"], indirect=True)
