7cebee79bb42a7eadd693a91ecf917aefcedce9f
Robin Gohmert <ayuto@web.de>
2025-02-12 13:53:40 +0100

    Fixed formatting and tests

--- drafthorse/models/note.py
+++ drafthorse/models/note.py
@@ -4,11 +4,13 @@ from .fields import StringField
 
 
 class IncludedNote(Element):
-    content_code: StringField = StringField(NS_RAM, "ContentCode", required=False, profile=EXTENDED)
-    content: StringField = StringField(
-        NS_RAM, "Content", required=False, profile=BASIC
+    content_code: StringField = StringField(
+        NS_RAM, "ContentCode", required=False, profile=EXTENDED
     )
-    subject_code: StringField = StringField(NS_RAM, "SubjectCode", required=False, profile=COMFORT)
+    content: StringField = StringField(NS_RAM, "Content", required=False, profile=BASIC)
+    subject_code: StringField = StringField(
+        NS_RAM, "SubjectCode", required=False, profile=COMFORT
+    )
 
     class Meta:
         namespace = NS_RAM
