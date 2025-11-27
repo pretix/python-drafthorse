e449f43e652803ff7e975c3c067573e615dd6629
Tobias Kuhn <tobias.kuhn.73@googlemail.com>
2025-11-27 20:01:12 +0100

    Fix languages field definition, adjust test and update readme (#87)
    * fix languages field definition, adjust test and update readme
    
    * Rename
    
    ---------
    
    Co-authored-by: Raphael Michel <michel@rami.io>

--- drafthorse/models/document.py
+++ drafthorse/models/document.py
@@ -80,7 +80,7 @@ class Header(Element):
         profile=EXTENDED,
         _d="Indikator Original/Kopie",
     )
-    languages: StringContainer = MultiStringField(
+    language = StringField(
         NS_RAM, "LanguageID", required=False, profile=EXTENDED
     )
     notes: Container = MultiField(IncludedNote)
